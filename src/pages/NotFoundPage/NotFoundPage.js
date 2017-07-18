096d7cdb613205934a9d6d7ec6667cad3317b7a2
Bakti Aditya <bakti.putra@traveloka.com>
2017-07-19 04:28:49 +0700

    Updating NotFound page

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import { PAGE_TITLE_PREFIX, PAGE_TITLE_SEP } from '../../shared/constants';
+import { slugify } from '../../shared/utils';
 import scss from './NotFoundPage.mod.scss';
+import scssString from './NotFoundPage.string.scss';
 
 class ErrorPage extends Component {
+  static displayName = 'ErrorPage';
+
   _style;
   _pageTitle = 'Page Not Found';
 
@@ -12,10 +17,10 @@ class ErrorPage extends Component {
     titleTag.innerHTML = `${PAGE_TITLE_PREFIX} ${PAGE_TITLE_SEP} ${this._pageTitle}`;
 
     // Create custom <style /> in <head />
-    let css = 'html { display: table; height: 100%; text-align: center; width: 100%; }';
-    css += 'body { display: table-cell; vertical-align: middle; margin: 2em auto; }';
+    const css = scssString;
     const head = document.head || document.getElementsByTagName('head')[0];
     this._style = document.createElement('style');
+    this._style.id = slugify(ErrorPage.displayName);
 
     this._style.type = 'text/css';
     if (this._style.styleSheet) {
@@ -38,7 +43,7 @@ class ErrorPage extends Component {
       <div>
         <h1 className={scss['headline']}>{this._pageTitle}</h1>
         <p className={scss['caption']}>Sorry, but the page you were trying to view does not
-          exist.</p>
+          exist. Head over to the <Link to='/'>home page</Link> to choose a new direction.</p>
       </div>
     );
   }
